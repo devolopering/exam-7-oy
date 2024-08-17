675f5065d5fafb90708715159f5e86503335ee89
ID22663 <gayratyoldoshboyev3@gmail.com>
2024-08-17 19:11:16 +0500

    sellect add

--- src/components/main/Filter.jsx
+++ src/components/main/Filter.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { IoChevronDown } from 'react-icons/io5';
 
-function Filter({sortBy, setSortBy}) {
+function Filter({ sortBy, setSortBy }) {
   return (
-    <div className='bg-[#D5F8CF] text-[#0BA42D]  px-5 pb-6 pt-7 mb-11'>
+    <div className='bg-[#D5F8CF] text-[#0BA42D] px-5 pb-6 pt-7 mb-11'>
       <div className='max-w-[1298px] mx-auto flex justify-between items-center font-hammersmith'>
         <h3 className='text-2xl font-normal'>Filter by:</h3>
-        <select name="sortby" value={sortBy} onChange={(e) => setSortBy(e.target.value)} className='border-none text-2xl  outline-none px-1 py-1 bg-transparent'>
-          <option value="sort">Sort by</option>
-          <option value="cheap">Cheap</option>
-          <option value="expensive">Expensive</option>
-        </select>
+        <div className="relative inline-flex items-center">
+          <IoChevronDown  id='select' className="absolute text-3xl font-bold left-1   text-[#0BA42D]"  />
+          <select
+          id='select'
+            name="sortby"
+            value={sortBy}
+            onChange={(e) => setSortBy(e.target.value)}
+            className="pl-8 border-none text-2xl outline-none px-1 py-1 bg-transparent appearance-none"
+          >
+            <option value="sort">Sort by</option>
+            <option value="cheap">Cheap</option>
+            <option value="expensive">Expensive</option>
+          </select>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Filter
+export default Filter;
