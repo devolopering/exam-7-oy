8aec852d186ad519608b1c5fe79e0cd2b40b71b8
ID22663 <gayratyoldoshboyev3@gmail.com>
2024-08-17 18:29:11 +0500

    sort size update

--- src/components/main/Filter.jsx
+++ src/components/main/Filter.jsx
@@ -5,7 +5,7 @@ function Filter({sortBy, setSortBy}) {
     <div className='bg-[#D5F8CF] text-[#0BA42D]  px-5 pb-6 pt-7 mb-11'>
       <div className='max-w-[1298px] mx-auto flex justify-between items-center font-hammersmith'>
         <h3 className='text-2xl font-normal'>Filter by:</h3>
-        <select name="sortby" value={sortBy} onChange={(e) => setSortBy(e.target.value)} className='border-none outline-none px-1 py-1 bg-transparent'>
+        <select name="sortby" value={sortBy} onChange={(e) => setSortBy(e.target.value)} className='border-none text-2xl  outline-none px-1 py-1 bg-transparent'>
           <option value="sort">Sort by</option>
           <option value="cheap">Cheap</option>
           <option value="expensive">Expensive</option>
